11d07ecbbd55379c0b9b7bc3b77b102a33312ec2
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-03-09 23:06:54 +0000

    current forecast complete

--- assets/scrip.js
+++ assets/scrip.js
@@ -1,33 +1,70 @@
 $(function () {
-var btn = document.getElementById('btn');
-var listGroup = $('.group-list');
-var currentForecastSection = document.body.children[1].children[0].children[1];
-var fiveDayForecastSection = document.body.children[1].children[1];
-var apiKey = 'bec4025b9704b04bb71486ebf08243fd';
+    var btn = document.getElementById('btn');
+    var listGroup = $('.group-list');
+    var currentSec = document.getElementById('current');
+    var fiveDaySec = document.getElementById('fiveday');
+    var apiKey = 'bec4025b9704b04bb71486ebf08243fd';
+    var addLI = $(listGroup).append('<li>');
+    var name1 = document.createElement('p');
+    var wind = document.createElement('p');
+    var temper = document.createElement('p');
+    var weathers = document.createElement('p');
+    var icon1 = document.createElement('img')
 
-
-    function currentWeather() {
-        var input = $('#location').innerHTML;
+    function currentWeather(e) {
+        e.preventDefault();
+        var input = $('.input').val();
         var currentForecast = 'https://api.openweathermap.org/data/2.5/weather?q=' + input + '&appid=' + apiKey + '&units=metric';
-        console.log('current ok');
+        console.log(currentForecast);
         console.log(input);
         fetch(currentForecast)
-        .then(function (r) {
-            return r.json();
-        })
-       // .then(console.log(data))
-    };
+            .then(function (r) {
+                console.log(r);
+                return r.json()
+            })
+            .then(function (d) {
+                console.log(d);
+                var weather = d.weather[0].main;
+                var temp = d.main.temp; // celcius
+                var windSpeed = d.wind.speed;  //meters per second
+                var name = d.name;
+                var iconCode = d.weather[0].icon;
+                weathers.textContent = weather;
+                temper.textContent = temp;
+                wind.textContent = windSpeed;
+                name1.textContent = name;
+                icon1.setAttribute('src', 'http://openweathermap.org/img/w/' + iconCode + '.png');
+                currentSec.appendChild(icon1)
+                currentSec.appendChild(name1);
+                currentSec.appendChild(wind);
+                currentSec.appendChild(temper);
+                currentSec.appendChild(weathers);
+                console.log(weather)
+            })
+        };
 
-    function fiveDay() {
-        var input = $('#location').innerHTML;
+    function fiveDay(e) {
+        e.preventDefault();
+        var input = $('.input').val();
         var fiveDayForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=' + input + '&appid=' + apiKey + '&units=metric';
-        console.log('five day ok');
+        console.log(fiveDayForecast);
         fetch(fiveDayForecast)
-        .then(function(r) {
-            return r.json();
-        })
-       // .then(console.log(data))
-    }
+            .then(function (r) {
+                console.log(r);
+                return r.json()
+            })
+            .then(function (d) {
+                console.log(d);
+                console.log(d)
+            })
+        };
+                function getAndSet() {
+                    localStorage.setItem
+                }
+            
+                btn.addEventListener('click', currentWeather)
+                btn.addEventListener('click', fiveDay)
 
-    btn.addEventListener('click', currentWeather(), fiveDay());
-});
+
+
+            })
